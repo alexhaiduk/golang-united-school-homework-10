7ae77083e787aed6809d37bdeaf152d745dce655
alexhaiduk <alexhaiduk82@gmail.com>
2022-06-30 15:39:37 +0300

    first test

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -18,8 +18,31 @@ main function reads host/port from env just for an example, flavor it following 
 */
 
 // Start /** Starts the web server listener on given host and port.
+/*
+Endpoints expected by tests:
+
+| METHOD | REQUEST                               | RESPONSE                      |
+|--------|---------------------------------------|-------------------------------|
+| GET    | `/name/{PARAM}`                       | body: `Hello, PARAM!`         |
+| GET    | `/bad`                                | Status: `500`                 |
+| POST   | `/data` + Body `PARAM`                | body: `I got message:\nPARAM` |
+| POST   | `/headers`+ Headers{"a":"2", "b":"3"} | Header `"a+b": "5"`           |
+
+If not defined in table:
+Request will be:
+* No body set
+* No headers set
+Response expected to have
+* Status: 200 OK
+* Empty body
+*/
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/name/{PARAM}", handleName).Methods(http.MethodGet)
+	router.HandleFunc("/bad", handleBad).Methods(http.MethodGet)
+	router.HandleFunc("/data", handleData).Methods(http.MethodPost)
+	router.HandleFunc("/headers", handleHeaders).Methods(http.MethodPost)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -35,4 +58,53 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func handleName(w http.ResponseWriter, r *http.Request) {
+	//code
+	param := mux.Vars(r)["PARAM"]
+	data := []byte("Hello, " + param + "!")
+	//w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func handleBad(w http.ResponseWriter, r *http.Request) {
+	//code
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func handleData(w http.ResponseWriter, r *http.Request) {
+	//code
+	var param []byte
+	//if count, err := r.Body.Read(param); err != nil {
+	//d, err := io.ReadAll(r.Body)
+	if _, err := r.Body.Read(param); err != nil {
+		log.Fatal(err)
+	}
+	//if count == 0 {
+	//	param = []byte("")
+	//}
+	data := []byte("I got message:\n") // + PARAM body
+	data = append(data, param...)
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func handleHeaders(w http.ResponseWriter, r *http.Request) {
+	//code
+	var a1, b1, result int
+	var err error
+	a := r.Header["a"][0]
+	b := r.Header["b"][0]
+	if a1, err = strconv.Atoi(a); err != nil {
+		log.Fatal(err)
+	}
+	if b1, err = strconv.Atoi(b); err != nil {
+		log.Fatal(err)
+	}
+	result = a1 + b1
+	w.Header().Add("a+b", string(result))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(""))
 }
